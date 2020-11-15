b80479caaa8c8754cc1fb721271627c638369a8e
JMS55 <47158642+JMS55@users.noreply.github.com>
2020-11-15 17:51:20 -0500

    Fix install.sh

--- infra/install.sh
+++ infra/install.sh
@@ -1,6 +1,7 @@
 cp Caddyfile /etc/caddy/
 cp chessmatch.service /etc/systemd/system/
+mkdir /etc/systemd/system/chessmatch.service.d/
 echo "[Service]" >> /etc/systemd/system/chessmatch.service.d/local.conf
-echo "Environment=\"SECRET_KEY_BASE=$(MIX_ENV=prod mix phx.gen.secret)"\" >> /etc/systemd/system/chessmatch.service.d/local.conf
+echo "Environment=\"SECRET_KEY_BASE=$(cd .. && MIX_ENV=prod mix phx.gen.secret)"\" >> /etc/systemd/system/chessmatch.service.d/local.conf
 systemctl enable caddy.service
 systemctl enable chessmatch.service
