4a75676ac4405fbdf9d28b2e9a50a5ee1df68a6e
JMS55 <47158642+JMS55@users.noreply.github.com>
2020-11-15 02:53:56 -0500

    Deploy to azure

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chessmatch.MixProject do
   def project do
     [
       app: :chessmatch,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
